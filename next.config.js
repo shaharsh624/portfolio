9ba8bff5539dd1f2ba230c72ed003506ed9dcd99
Harsh Shah <harshdev624@gmail.com>
2023-07-07 15:37:02 +0530

    Disabled ESLint

--- next.config.js
+++ next.config.js
@@ -3,10 +3,14 @@ const nextConfig = {
   images: {
     domains: ["images.pexels.com", "github.com", "raw.githubusercontent.com"],
   },
-
   compiler: {
     // Enables the styled-components SWC transform
     styledComponents: true,
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
   },
 };
 
